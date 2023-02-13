d068826750cee0f77e2d4026b40a7ff95e7c71c4
abhiram200 <72096969+abhiram200@users.noreply.github.com>
2023-02-13 20:21:48 +0530

    Add files via upload
    This code checks for the mouse location every 5 seconds for 64 times. You can download the code and modify it according to your needs. It also uses the tkinter library which is used to show the timer in pop up view.

---
+++ mouse_loc_track.py
@@ -0,0 +1,28 @@
+import pyautogui
+import tkinter as tk
+
+def show_timer(root, label, count_down):
+    if count_down >= 0:
+        label.config(text="Timer: {} seconds".format(count_down))
+        root.after(1000, show_timer, root, label, count_down - 1)
+    else:
+        root.destroy()
+
+def start_timer():
+    root = tk.Tk()
+    root.geometry("200x100")
+    root.title("Timer")
+    label = tk.Label(root, text="Timer: 5 seconds")
+    label.pack()
+    show_timer(root, label, 5)
+    root.mainloop()
+
+
+
+count = 0
+
+while count < 64:
+    start_timer()
+    x, y = pyautogui.position()
+    print("Mouse is at (x, y): ({}, {})".format(x, y))
+    count = count + 1
